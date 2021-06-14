7589c7ba873914b38b9678d230d1a4502b710559
Dawid Konojacki <dawid.konojacki@sb-betting.com>
2021-06-14 16:36:27 +0200

    set() method for update User property

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import { User } from "./models/User";
 const user = new User({name: "Dawid", age: 31});
 console.log(user.get('name'));
 console.log(user.get('age'));
+user.set({name: 'Robert '});
+console.log(user.get('name'));
