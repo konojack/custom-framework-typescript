5b6afc30eef20285024258da2751a5f35a2cb2a2
konojack <konojack@gmail.com>
2021-06-15 14:54:41 +0200

    Create 'on' method for register events

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { User } from "./models/User";
+import { User } from './models/User';
 
-const user = new User({name: "Dawid", age: 31});
+const user = new User({ name: 'Dawid', age: 31 });
 console.log(user.get('name'));
 console.log(user.get('age'));
-user.set({name: 'Robert '});
+user.set({ name: 'Robert ' });
 console.log(user.get('name'));
+user.on('click', () => {
+  console.log('ssd');
+});
+user.on('click', () => {});
+console.log(user.events['click'][0]());
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,18 +1,27 @@
 interface UserProps {
-    name?: string;
-    age?: number;
+  name?: string;
+  age?: number;
 }
+
+type Callback = () => void;
 
 export class User {
-    constructor(private data: UserProps) {}
+  events: { [key: string]: Callback[] } = {};
+  constructor(private data: UserProps) {}
 
-    get(propName: string): (number | string) {
-        return this.data[propName]
-    }
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
 
-    set(update: UserProps): void {
-        Object.assign(this.data, update);
-    }
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback) {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
 }
 
-new User({name: 'Dawid', age: 31});
+new User({ name: 'Dawid', age: 31 });
