fdb0dd8759e465d461e4c243980a18e2b611dcc6
konojack <konojack@gmail.com>
2021-06-16 11:56:40 +0200

    Adding Fetch method for get data from JSON-SERVER

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'Dawid', age: 31 });
-console.log(user.get('name'));
-console.log(user.get('age'));
-user.set({ name: 'Robert ' });
-console.log(user.get('name'));
-user.on('click', () => {
-  console.log('ssd');
+axios.post('http://localhost:3000/users', {
+  name: 'myName',
+  age: 20,
 });
-user.on('click', () => {
-  console.log('click2');
-});
-user.on('change', () => {
-  console.log('change');
-});
-user.trigger('change');
-user.trigger('click');
