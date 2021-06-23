c5c85d8547e62a4fdde079372074390011fb8439
konojack <konojack@gmail.com>
2021-06-23 15:24:20 +0200

    Add Attributes class for get and set properties

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,22 +12,6 @@ const rootUrl = 'https://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync(rootUrl);
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
 }
 
 new User({ name: 'Dawid', age: 31 });
