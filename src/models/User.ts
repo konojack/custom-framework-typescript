84836db9faf7f0d71fa8c3c5dfbd6c997ef60ca2
konojack <konojack@gmail.com>
2021-06-23 14:42:15 +0200

    Add Sync class for handling requests

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,17 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = 'https://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync(rootUrl);
 
   constructor(private data: UserProps) {}
 
@@ -24,23 +27,6 @@ export class User {
     const handlers = this.events[eventName] || [];
     handlers.push(callback);
     this.events[eventName] = handlers;
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post(`http://localhost:3000/users`, this.data);
-    }
   }
 }
 
