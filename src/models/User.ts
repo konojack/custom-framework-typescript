291dd2d9ce7f9fabcdc81128ac1e685a8d819cfd
konojack <konojack@gmail.com>
2021-06-25 00:20:41 +0200

    Add set() and fetch() methods for a User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from 'axios';
 import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
@@ -29,6 +30,22 @@ export class User {
 
   get trigger() {
     return this.events.trigger;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
   }
 }
 
