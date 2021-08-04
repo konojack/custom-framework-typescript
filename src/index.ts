a4154a07b415e25d6814a4d07db1f10e41719178
konojack <konojack@gmail.com>
2021-08-04 13:48:10 +0200

    Adding methods to User.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { User } from './models/User';
 import { UserForm } from './views/UserForm';
 const user = User.buildUser({ name: 'NAME', age: 20 });
-const userForm = new UserForm(document.querySelector('#root'), user);
-userForm.render();
+const root = document.querySelector('#root');
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root does not exist');
+}
